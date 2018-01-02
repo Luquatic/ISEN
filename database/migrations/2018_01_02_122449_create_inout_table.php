fc5235dde048046c69360cac32a0d2ae9ba3394f
Luquatic <jesseyfransen@live.nl>
2018-01-02 12:36:45 +0100

    added create tables

---
+++ database/migrations/2018_01_02_122449_create_inout_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInoutTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('inout', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('in/out');
+            $table->timestamps();
+            $table->rememberToken();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('inout');
+    }
+}
