a5eb87026c2d08a36885bdf75e355ec1fc6d612d
Luquatic <jesseyfransen@live.nl>
2018-01-04 15:46:43 +0100

    updated migrations

--- database/migrations/2018_01_02_122449_create_inout_table.php
+++ database/migrations/2018_01_02_122449_create_inout_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateIn_outTable extends Migration
+class CreateInoutTable extends Migration
 {
     /**
      * Run the migrations.
