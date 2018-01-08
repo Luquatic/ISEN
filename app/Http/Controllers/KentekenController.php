0a268b3cefc160d757ef452cd05ff87f50982076
Luquatic <jesseyfransen@live.nl>
2018-01-08 18:11:25 +0100

    small changes

--- app/Http/Controllers/KentekenController.php
+++ app/Http/Controllers/KentekenController.php
@@ -11,6 +11,6 @@ class KentekenController extends Controller
     public function index() {
         $kenteken = Kenteken::all();
 
-        return view('kentekens.index', compact('kenteken'));
+        return view('layouts.home', compact('kenteken'));
     }
 }
