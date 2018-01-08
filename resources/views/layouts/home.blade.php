0fd8d26848d0d98d6899e295f535aeb9ecb16d03
Luquatic <jesseyfransen@live.nl>
2018-01-08 17:59:48 +0100

    small changes

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -58,13 +58,15 @@
 <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
 <![endif]-->
 
-<h1>Alle kentekens</h1>
+<div class="container">
+    <h1>Alle kentekens</h1>
 
-<ul>
-@foreach ($kenteken as $kentekens)
-    {{ $kentekens }}
-@endforeach
-</ul>
+    <ul>
+        @foreach ($kenteken as $kentekens)
+            {{ $kentekens }}
+        @endforeach
+    </ul>
+</div>
 
 <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
 <script>
