1b5cd0abb2233974350cc001fc42449f4623c167
Luquatic <jesseyfransen@live.nl>
2018-01-15 17:25:07 +0100

    changed name and content

--- app/Http/Controllers/KentekensController.php
+++ app/Http/Controllers/KentekensController.php
@@ -11,6 +11,6 @@ class KentekensController extends Controller
     public function index() {
         $kentekens = Kenteken::all();
 
-        return view('layouts.home', compact('kentekens'));
+        return view('layouts.kenteken', compact('kentekens'));
     }
 }
