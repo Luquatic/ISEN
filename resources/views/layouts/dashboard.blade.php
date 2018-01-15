b8b77b9a88476434ef6b4e124fbced2c5842dfac
Luquatic <jesseyfransen@live.nl>
2018-01-15 17:56:14 +0100

    changed content

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -100,6 +100,9 @@
 
 <div id="table_div" style="width: 89%; margin-left: 11%"></div>
 
+<a href="https://github.com/google/material-design-lite/blob/mdl-1.x/templates/dashboard/" target="_blank" id="view-source" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--colored mdl-color-text--white">View Source</a>
+<script src="https://github.com/Luquatic"></script>
+
 <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
 <script>
     (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
