f4934177b3b881ccb4ea3e16d803e96b74e9683c
Luquatic <jesseyfransen@live.nl>
2018-01-15 17:55:13 +0100

    changed content

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -98,10 +98,7 @@
     </div>
 </div>
 
-<div id="table_div" style="width: 90%; margin-left: 11%"></div>
-
-<a href="https://github.com/google/material-design-lite/blob/mdl-1.x/templates/dashboard/" target="_blank" id="view-source" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--colored mdl-color-text--white">View Source</a>
-<script src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+<div id="table_div" style="width: 89%; margin-left: 11%"></div>
 
 <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
 <script>
